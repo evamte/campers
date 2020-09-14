64e3154aa609b99d82ec569273e9e526a17e825d
Student-KBH <student-kbh@Student-KBHs-MacBook-Pro.local>
2020-09-14 15:58:16 +0200

    tried to add sections - not working

--- js/script.js
+++ js/script.js
@@ -1,10 +1,35 @@
 const link = "https://spreadsheets.google.com/feeds/list/1eCMfGSpIylwq83kYmOGm6OkiDgJRlqHCrMDuH1qG5D0/od6/public/values?alt=json";
 window.addEventListener("DOMContentLoaded", getData);
 
+//init=getData
+//data=handleData
 function getData() {
     fetch(link)
     .then(res => res.json())
     .then(handleData);
+    then( function (handleData) {
+    locationRecieved(handleData)
+    }
+    )
+}
+
+getData();
+
+function locationRecieved(loc) {
+    createSections(loc);
+    fetchProducts();
+}
+
+
+function createSections(regions) {
+    regions.forEach(gsx$region => {
+        const section = document.createElement("section");
+        section.setAttribute("id", region);
+        const h2 = document.createElement("h2");
+        h2.textContent = region;
+        section.appendChild(h2);
+        document.querySelector(".productlist").appendChild(section);
+    })
 }
 
 function handleData(data) {
@@ -12,6 +37,26 @@ function handleData(data) {
     console.log("myData - console");
     console.log(myData);
     myData.forEach(showData);
+}
+
+function fetchProducts() {
+}
+
+//fetch data
+fetch(link)
+    .then(function (response) {
+        console.log(response)
+        return response.json();
+    })
+
+    .then(function (handleData) {
+
+        dataReceived(handleData);
+    })
+
+function dataReceived(products) {
+    //loop through products
+    products.forEach(showData)
 }
 
 function showData(singleRowData) {
@@ -24,6 +69,8 @@ function showData(singleRowData) {
     h3.textContent = singleRowData.gsx$name.$t;
     const h4 = clone.querySelector('h4');
     h4.textContent = singleRowData.gsx$address.$t;
-
     document.querySelector('main').appendChild(clone);
+
 }
+
+
