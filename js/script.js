a22974ab8440bc64113d54df03b9e1f60c44effc
Zuzanka <zuz@laptop-0jopo9t4.kea.local>
2020-09-15 13:55:54 +0200

    added navigation and sections - this time it works!

--- js/script.js
+++ js/script.js
@@ -1,83 +1,72 @@
-const link = "https://spreadsheets.google.com/feeds/list/1eCMfGSpIylwq83kYmOGm6OkiDgJRlqHCrMDuH1qG5D0/od6/public/values?alt=json";
+const link =
+  "https://spreadsheets.google.com/feeds/list/1eCMfGSpIylwq83kYmOGm6OkiDgJRlqHCrMDuH1qG5D0/od6/public/values?alt=json";
 window.addEventListener("DOMContentLoaded", getData);
 
 //init=getData
 //data=handleData
 function getData() {
-    fetch(link)
-    .then(res => res.json())
-    .then(handleData);
-    then( function (handleData) {
-    locationRecieved(handleData)
+  fetch(link)
+    .then((res) => res.json())
+    .then(dataRecieved);
+}
+
+function dataRecieved(regions) {
+  console.log(regions);
+  const data = regions.feed.entry;
+  createSections(data);
+  createNavigation(data);
+  showProducts(data);
+}
+
+function createNavigation(data) {
+  let regions = [];
+  data.forEach((location) => {
+    if (!regions.includes(location.gsx$region.$t)) {
+      regions.push(location.gsx$region.$t);
     }
-    )
-}
-
-getData();
-
-function regionsRecieved(regions) {
-    createSections(regions);
-    createNavigation(regions)
-    fetchProducts();
-}
-
-function createNavigation(regions) {
-  categories.forEach(gsx$region => {
-    console.log(region)
+  });
+  regions.forEach((location) => {
     const a = document.createElement("a");
-    a.textContent = region;
-    a.setAttribute("href", `#${regions}`)
+    a.textContent = location;
+    a.setAttribute("href", `#${location}`);
     document.querySelector("nav").appendChild(a);
-  })
+  });
 }
 
-function createSections(regions) {
-    regions.forEach(gsx$region => {
-        const section = document.createElement("section");
-        section.setAttribute("id", region);
-        const h2 = document.createElement("h2");
-        h2.textContent = region;
-        section.appendChild(h2);
-        document.querySelector(".productlist").appendChild(section);
-    })
+function createSections(data) {
+  let regions = [];
+  data.forEach((location) => {
+    if (!regions.includes(location.gsx$region.$t)) {
+      regions.push(location.gsx$region.$t);
+    }
+  });
+  regions.forEach((location) => {
+    const section = document.createElement("section");
+    section.setAttribute("id", location);
+    const h2 = document.createElement("h2");
+    h2.textContent = location;
+    section.appendChild(h2);
+    document.querySelector(".productlist").appendChild(section);
+  });
 }
 
-function handleData(data) {
-    const myData = data.feed.entry;
-    console.log("myData - console");
-    console.log(myData);
-    myData.forEach(showData);
-}
-
-function fetchProducts() {
-}
-
-//fetch data
-fetch(link)
-    .then(function (response) {
-        console.log(response)
-        return response.json();
-    })
-
-    .then(function (handleData) {
-
-        dataReceived(handleData);
-    })
-
-function dataReceived(products) {
-    //loop through products
-    products.forEach(showData)
+function showProducts(products) {
+  //loop through products
+  products.forEach(showData);
 }
 
 function showData(singleRowData) {
-    console.log('singleRowData - console');
-    console.log(singleRowData.gsx$name.$t);
+  console.log("singleRowData - console");
+  console.log(singleRowData.gsx$name.$t);
 
-    const template = document.querySelector('template').content;
-    const clone = template.cloneNode(true);
-    const h3 = clone.querySelector('h3');
-    h3.textContent = singleRowData.gsx$name.$t;
-    const h4 = clone.querySelector('h4');
-    h4.textContent = singleRowData.gsx$address.$t;
-    document.querySelector('main').appendChild(clone);
+  const template = document.querySelector("template").content;
+  const clone = template.cloneNode(true);
+  const h3 = clone.querySelector("h3");
+  h3.textContent = singleRowData.gsx$name.$t;
+  const h4 = clone.querySelector("h4");
+  h4.textContent = singleRowData.gsx$address.$t;
+  //document.querySelector("main").appendChild(clone);
+  document
+    .querySelector(`section#${singleRowData.gsx$region.$t}`)
+    .appendChild(clone);
 }
